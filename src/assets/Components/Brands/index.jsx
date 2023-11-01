7dde24c906e5e549900f608beaf664a7ce546959
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-11-01 21:31:27 +0400

    slide section and brands comp completed

--- src/assets/Components/Brands/index.jsx
+++ src/assets/Components/Brands/index.jsx
@@ -1,13 +1,53 @@
-const Brands = () => {
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const BrandsSlider = () => {
+  const settings = {
+    infinite: true, // Sonsuz döngü etkinleştirildi
+    slidesToShow: 5, // Aynı anda görünen SVG sayısı
+    slidesToScroll: 1, // Kaydırma işlemi sırasında kaç SVG'nin ilerleyeceği
+    autoplay: true, // Otomatik oynatma etkinleştirildi
+    autoplaySpeed: 1000, // Otomatik oynatma hızı (ms cinsinden)
+  };
+
   return (
-    <div>
-      <img src="./01.svg" alt="" />
-      <img src="./02.svg" alt="" />
-      <img src="./03.svg" alt="" />
-      <img src="./04.svg" alt="" />
-      <img src="./05.svg" alt="" />
+    <div className="container w-100 mt-5 py-5 align-items-center">
+      <Slider {...settings}>
+        <div>
+          <img src="./01.svg" alt="" />
+        </div>
+        <div>
+          <img src="./02.svg" alt="" />
+        </div>
+        <div>
+          <img src="./03.svg" alt="" />
+        </div>
+        <div>
+          <img src="./04.svg" alt="" />
+        </div>
+        <div>
+          <img src="./05.svg" alt="" />
+        </div>
+        <div>
+          <img src="./01.svg" alt="" />
+        </div>
+        <div>
+          <img src="./02.svg" alt="" />
+        </div>
+        <div>
+          <img src="./03.svg" alt="" />
+        </div>
+        <div>
+          <img src="./04.svg" alt="" />
+        </div>
+        <div>
+          <img src="./05.svg" alt="" />
+        </div>
+      </Slider>
     </div>
   );
 };
 
-export default Brands;
+export default BrandsSlider;
